cfb5aaaf3b7000433766b318b084a5f77db1c279
Motor Bolts <motorbolts@gmail.com>
2015-10-11 15:04:19 -0500

    Added CRLift Program
    Adds functionality to the collector lift

--- ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurPrograms/CRTest.java
+++ ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/OurPrograms/CRTest.java
@@ -1,4 +1,4 @@
-package com.qualcomm.ftcrobotcontroller.opmodes;
+package com.qualcomm.ftcrobotcontroller.opmodes.OurPrograms;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.Servo;
